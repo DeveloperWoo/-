e01d6fa8f7ceca3e88f8a06128de44208c257e24
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-02 19:33:30 -0500

    Create Comment.js

---
+++ JSX_Basic/Comment.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  render() {
+    return (
+    <div>
+      {/*
+      multi
+      line
+      comment 
+    */}
+        <h1 // single line comment 
+        >
+      Hello World! 
+      </h1>
+    </div>)
+    ; 
+  }
+}
+
+export default App;
