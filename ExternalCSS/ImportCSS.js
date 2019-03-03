76a365dd465e1a96dadc5ba4d45e24dfe8ed25f7
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-02 19:24:03 -0500

    Update and rename importCSS.js to ImportCSS.js

--- ExternalCSS/ImportCSS.js
+++ ExternalCSS/ImportCSS.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class ImportCSS extends Component {
   render() {
     return <div className="App">Hello World!</div>;
   }
