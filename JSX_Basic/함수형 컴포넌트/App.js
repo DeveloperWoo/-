0adfa57ef0bcee4e821d1d94c95f0ff0a5d7ae94
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-03 17:07:04 -0500

    Create App.js

---
+++ JSX_Basic/함수형 컴포넌트/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import MyName from './MyName';
+
+class App extends Component {
+  render() {
+    return <MyName name="Hyunju" />;
+  }
+}
+
+export default App;
