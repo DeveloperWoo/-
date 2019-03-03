8d5b12165eb7b1147bbf5bc5a27422bb135d29b4
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-02 19:13:37 -0500

    Create css.js

---
+++ css.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const style = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '36px'
+    };
+    return (
+      <div style={style}>
+      Hello World!
+      </div>
+    );
+  }
+}
+
+export default App;
